c30812108ae9ca21189f34c7e3eff694cad84a3d
micziz <miczicontent@gmail.com>
2022-07-04 12:12:13 +0200

    Start making it

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+    @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+
+    #title{
+        text-align: center;
+        font-family: 'VT323', monospace;
+        margin-top: 30px;        
+    }
+</style>
+
+<head>
+    <title>My Website</title>
+</head>
+
+<h1 id="title" class=title>Hi! Im micziz! Welcome to my website!</h1>
+
