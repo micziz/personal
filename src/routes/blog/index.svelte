79b5c58de75edc17a11d319a648e3cfdccb94059
micziz <miczicontent@gmail.com>
2022-07-06 23:51:17 +0200

    First publick post

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,5 +1,6 @@
 <script>
     let blogPost = [
+        {id: "the-revolution-of-vite", name: "The Revolution Of Vite"}
     ]
 </script>
 
@@ -17,6 +18,19 @@
         font-family: 'VT323', monospace;
         text-align: center;
     }
+
+    #projects{
+        text-align: center;
+        font-size: 30px;
+        font-family: 'VT323', monospace;
+    }
+
+    #project-title{
+        text-align: center;
+        margin-top: 25px;
+        margin-bottom: 25px;
+        font-family: 'VT323', monospace;
+    }
 </style>
 
 <h1 class="title is-1">Wlecome to my blog!</h1>
@@ -26,3 +40,15 @@
 <p>These will mostly be releted to web development, but also other things...</p>
 
 <p>Thanks for visiting!!!!</p>
+
+<h2 class="title is-2" id="project-title">Recent Posts</h2>
+
+<div id="projects">
+    <ul>
+        {#each blogPost as post}
+            <li><a target="_blank" href="https://www.github.com/{post.id}" id="btn-projects" class="button is-large is-rounded is-black is-outlined">
+                {post.name}
+            </a></li>
+        {/each}
+    </ul>
+</div>
---
+++ src/routes/blog/the-revolution-of-vite.svelte
@@ -0,0 +1,100 @@
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+    @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+
+    h1{
+        font-family: 'VT323', monospace;
+        text-align: center;
+    }
+
+    h3{
+        margin-top: 15px;
+        font-family: 'VT323', monospace;
+        text-align: center;
+    }
+
+    p{
+        font-family: 'VT323', monospace;
+        text-align: center;
+    }
+
+    #conclusion{
+        margin-bottom: 50px;
+    }
+</style>
+
+
+<h1 class="title is-1">The revoution of vite!</h1>
+
+<p>
+    Vite! French word for "quick", pronounced /vit/, like "veet", is a revolutionary build tool.
+</p>
+<p>
+    It was created by even you blah blah blah..., we all know this stuff, but i would like to point to 3 characteristics that i really like about it!
+</p>
+
+
+<h3 class="title is-3">1-Convetion over configuration</h3>
+
+<p>
+    As developers, we dont't want to mess with config files, we need to write code and solve problems.
+</p>
+<p>
+    Vite solves this, respecting conventions, instead of letting you mess around. Mind you, you can do that, but it's not reccomended and desigend so you don't screw up.
+</p>
+<p>
+    I was to afraid to touch svelte beacause of the module bundlers and config files, and now, this blog is powered by svelte and vite (thanks sveltekit).
+</p>
+
+<h3 class="title is-3">2-It's all automatic.</h3>
+
+<p>
+    If i add dependencies, not only does the load times not slow down, but it automaticlly updates the site, even if it's running.
+    I love it, and cloud not live without it... 
+</p>
+
+<h3 class="title is-3">3-It's available everywhere</h3>
+
+<p>
+    This is the full list of frameworks/libraries vite officially supports (taken from amazing-svlete that can be found <a href="https://github.com/vitejs/awesome-vite#…">here</a>)
+</p>
+
+<p>
+    Vanilla <a href="https://github.com/vitejs/vite/tree/main/packages/create-vite">(official package)</a>
+</p>
+<p>
+    Vue 3 <a href="https://github.com/vitejs/vite/tree/main/packages/plugin-vue">(official package)</a>
+</p>
+<p>
+    Vue 2 <a href="https://github.com/underfin/vite-plugin-vue2">(official package)</a>
+</p>
+<p>
+    React <a href="https://github.com/vitejs/vite/tree/main/packages/plugin-react">(official package)</a>
+</p>
+<p>
+    Svelte <a href="https://github.com/sveltejs/vite-plugin-svelte">(official package)</a> (also <a href="https://github.com/sveltejs/kit">sveltekit</a>)
+</p>
+<p>
+    Solid <a href="https://github.com/amoutonbrady/vite-plugin-solid">(official package)</a>
+</p>
+<p>
+    Elm <a href="https://github.com/hmsk/vite-plugin-elm">(official package)</a>
+</p>
+<p>
+    Angular <a href="https://github.com/nxext/nx-extensions/tree/main/packages/angular/vite">(official package)</a>
+</p>
+<p>
+    Electron <a href="https://github.com/alex8088/electron-vite">(official package)</a>
+</p>
+<p>
+    <strong>PLUS</strong>
+</p>
+<p>
+    Other community supported are available too. Example, tauri works, although with some configuration.
+</p>
+
+<h3 class="title is-3">Conclusion</h3>
+
+<p id="conclusion">
+    I love vite, and i will continue to utilize it and hope you will start utilize it too.
+</p>
