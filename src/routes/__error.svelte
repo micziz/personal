3d003f4479afe67ed1f1119a0c5e8109d73a3b09
micziz <miczicontent@gmail.com>
2022-07-06 22:57:51 +0200

    404

---
+++ src/routes/__error.svelte
@@ -0,0 +1,11 @@
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+    @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+    
+    h1{
+        font-family: 'VT323', monospace;
+        text-align: center;
+    }
+</style>
+
+<h1 class="title is-1">Seems like this page doesent exist</h1>
