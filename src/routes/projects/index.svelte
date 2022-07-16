45084e79fef06e1a3774ffc33c32c360c35172cd
micziz <miczicontent@gmail.com>
2022-07-16 16:41:08 +0200

    re make rplg

---
+++ src/routes/projects/index.svelte
@@ -0,0 +1,34 @@
+<style>
+    h1{
+        text-align: center;
+        margin-bottom: 30px;
+    }
+
+    #projects{
+        text-align: center;
+        font-size: 30px;
+        font-family: 'VT323', monospace;
+    }
+
+    #btn-projects{
+        margin-bottom: 15px;
+    }
+</style>
+
+<script>
+    let projects = [
+        { id: "rplg", name: "random programming language generator"},
+    ]
+</script>
+
+<h1 class="title is-2">Some web projects i have made!</h1>
+
+<div id="projects">
+    <ul>
+        {#each projects as project}
+            <li><a target="_blank" href="projects/{project.id}" id="btn-projects" class="button is-large is-rounded is-black is-outlined">
+                {project.name}
+            </a></li>
+        {/each}
+    </ul>
+</div>  
---
+++ src/routes/projects/rplg/index.svelte
@@ -0,0 +1,40 @@
+<script>
+    function generate(){
+        let languages = ["C", "C++", "Java", "Python", "JavaScript", "Objective-C", "Matlab", "Ruby", "Lua", "PHP", "Swift", "Go", "Perl", "R", "Scala", "Haskell", "Clojure", "ClojureScript", "Rust", "Dart", "Elixir", "Erlang", "F#", "Fortran", "Groovy", "Haskell", "Kotlin", "Lisp", "Lua", "OCaml", "Scheme", "Smalltalk", "TypeScript", "Visual Basic", "Cobol", "Prolog", "Ada", "Assembly", "C#", "D", "Go", "Java", "JavaScript", "Objective-C", "Matlab", "Ruby", "Lua", "PHP", "Swift", "Go", "Perl", "R", "Scala", "Haskell", "Clojure", "ClojureScript", "Rust", "Dart", "Elixir", "Erlang", "F#", "Fortran", "Groovy", "Haskell", "Kotlin", "Lisp", "Lua", "OCaml", "Scheme", "Smalltalk", "TypeScript", "Visual Basic", "Cobol", "Prolog", "Ada", "Assembly", "C#", "D", "Go", "Java", "JavaScript", "Objective-C", "Matlab", "Ruby", "Lua", "PHP", "Swift", "Go", "Perl", "R", "Scala", "Haskell", "Clojure", "ClojureScript", "Rust", "Dart", "Elixir", "Erlang", "F#", "Fortran", "Groovy", "Haskell", "Kotlin", "Lisp", "Lua", "OCaml", "Scheme", "Smalltalk", "TypeScript", "Visual Basic", "Cobol", "Prolog", "Ada", "Assembly", "C#", "D", "Go", "Java", "JavaScript", "Objective-C", "Matlab", "Ruby", "Lua", "PHP", "Swift", "Go", "Perl", "R", "Scala", "Haskell", "Clojure", "ClojureScript", "Rust", "Dart", "Elixir", "Erlang", "F#", "Fortran", "Groovy", "Haskell", "Kotlin", "Lisp", "Lua", "OCaml", "Scheme", "Smalltalk", "TypeScript", "Visual Basic", "Cobol", "Prolog", "Ada", "Assembly", "C#", "D", "Go", "Java", "JavaScript", "Objective-C", "Matlab", "Ruby", "Lua", "PHP", "Swift", "Go", "Perl", "R"];
+        let language = languages[Math.floor(Math.random()*languages.length)];
+        return language;
+    }
+
+    let lang = generate();
+
+    function generateNew(){
+        lang = generate();
+    }
+</script>
+
+<style>
+    h1 {
+        font-family: 'VT323', monospace;
+        text-align: center;
+    }
+
+    p {
+        font-family: 'VT323', monospace;
+        text-align: center;
+    }
+
+    #generate{
+        margin-top: 25px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+
+<h1 class="title is-2">Random Programming Language Generator</h1>
+
+<p>Language = {lang}</p>
+
+<div id="generate">
+    <button on:click={generateNew}>Generate</button>
+</div>
